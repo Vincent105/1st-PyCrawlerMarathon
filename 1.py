d163be19b6e1cc2641bf316df66f166f42d440e0
vincent105 <vincent1050917@gmail.com>
2019-11-27 21:53:06 +0800

    1127

--- 1.py
+++ 1.py
@@ -1,8 +1,13 @@
 import xmltodict
-with open('./data/64_72hr_CH.xml', encoding='UTF-8-sig') as fd:
+
+with open('./data/64_72hr_CH.xml', encoding='UTF-8') as fd:
     doc = dict(xmltodict.parse(fd.read()))
 
+sect_with_tempers = doc["cwbopendata"]["dataset"]["locations"]["location"]
 
-locations = doc["cwbopendata"]["dataset"]["locations"]["location"]
-
-print(doc["cwbopendata"]["dataset"]["locations"]["location"])
+for sect_with_temper in sect_with_tempers:
+    if sect_with_temper['locationName'] == '鹽埕區':
+        for weatherElement in sect_with_temper['weatherElement']:
+            if weatherElement['description'] == '溫度':
+                for time in weatherElement['time']:
+                    print(time['dataTime'], time['elementValue']['value'])
