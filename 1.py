bb9d28cb974fc6f16e9f57b323b942833200e159
vincent105 <vincent1050917@gmail.com>
2019-11-27 21:37:59 +0800

    1127

--- 1.py
+++ 1.py
@@ -1,1 +1,8 @@
-import sys; print(sys.path)
+import xmltodict
+with open('./data/64_72hr_CH.xml', encoding='UTF-8-sig') as fd:
+    doc = dict(xmltodict.parse(fd.read()))
+
+
+locations = doc["cwbopendata"]["dataset"]["locations"]["location"]
+
+print(doc["cwbopendata"]["dataset"]["locations"]["location"])
