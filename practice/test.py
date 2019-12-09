a1a9e787ecb6a2ad681b5c990195812e987df5f2
vincent105 <vincent1050917@gmail.com>
2019-12-09 22:17:48 +0800

    1209

--- practice/test.py
+++ practice/test.py
@@ -1,19 +1,21 @@
 import requests
-import collections
+from collections import Counter
 from bs4 import BeautifulSoup
 
 url = 'https://www.ettoday.net/news/news-list.htm'
 r = requests.get(url)
 
-soup = BeautifulSoup(r.text.replace(u'\u3000', '　'), "html.parser")
+soup = BeautifulSoup(r.text, "html.parser")
 articles = {}
 count = 0
 
 for d in soup.find(class_="part_list_2").find_all('h3'):
     # print(d.find(class_="date").text, d.find_all('em')[-1].text , d.find_all('a')[-1].text)
 
-    articles[count] = ({'date': d.find(class_="date").text,
-                        'title': d.find_all('a')[-1].text.replace(u'\u3000', '　'),
-                        'tag': d.find_all("em")[-1].text})
+    articles[count] = d.find_all("em")[-1].text
     count += 1
-print(articles)
+# print(articles)
+
+max_count_tag = Counter(articles.values())
+print(max_count_tag.most_common(1)[
+      0][0], "類別的文章最多,合計有", max_count_tag.most_common(1)[0][1], "篇")
