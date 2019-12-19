176ede9d21ddcd8ea5b3fe46e02aadba20409248
Vincent105 <vincent1050917@gmail.com>
2019-12-19 17:10:43 +0800

    1219

--- practice/lxml_013_Elements_contain_text.py
+++ practice/lxml_013_Elements_contain_text.py
@@ -5,3 +5,11 @@ root.text = "TEXT"
 
 print(root.text)
 print(etree.tostring(root))
+
+html = etree.Element("html")
+body = etree.SubElement(html, "body")
+body.text = "TEXT"
+
+print(etree.tostring(html))
+
+br = etree.SubElement(body, "br")
