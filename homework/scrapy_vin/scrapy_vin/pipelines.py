01af402e593e39d682bba1f3ca38900e2af61de4
vincent105 <vincent1050917@gmail.com>
2020-01-01 11:19:02 +0800

    0101

--- homework/scrapy_vin/scrapy_vin/pipelines.py
+++ homework/scrapy_vin/scrapy_vin/pipelines.py
@@ -4,20 +4,50 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+import os
+import json
 
-import json 
-class ScrapyVinPipeline(object):            
+from pathlib import Path
+from datetime import datetime
+
+
+class ScrapyVinPipeline(object):
     def open_spider(self, spider):
-        self.file = open('items.jl', 'w')
+        
+        self.start_crawl_datetime = datetime.now().strftime('%Y%m%dT%H_%M%S')
+        self.dir_path = Path(__file__).resolve().parents[1] / 'crawled_data'
+        self.runtime_file_path = str(self.dir_path / '.tmp.json.swp')
+
+        if not self.dir_path.exists():
+            self.dir_path.mkdir(parents=True)
+        spider.log(
+            'Create temp file for store JSON - {}'.format(self.runtime_file_path))
+
+        self.runtime_file = open(self.runtime_file_path, 'w+', encoding='utf8')
+        self.runtime_file.write('[\n')
+        self._first_item = True
 
     def process_item(self, item, spider):
-        object_ = json.dump(dict(item)) + "\n"
-        self.write(object_)
+
+        if not isinstance(item, dict):
+            item = dict(item)
+
+        if self._first_item:
+            self._first_item = False
+        else:
+            self.runtime_file.write(',\n')
+
+        self.runtime_file.write(json.dumps(item, ensure_ascii=False))
         return item
 
     def close_spider(self, spider):
-        self.file.close()
+        self.end_crawl_datetime = datetime.now().strftime('%Y%m%dT%H_%M%S')
 
-
-
- 
+        self.runtime_file.write('\n]')
+        self.runtime_file.close()
+        
+        self.store_file_path = self.dir_path / '{}_{}.json'.format(self.start_crawl_datetime,
+                                                                   self.end_crawl_datetime)
+        self.store_file_path = str(self.store_file_path)
+        os.rename(self.runtime_file_path, self.store_file_path)
+        spider.log('Save result at {}'.format(self.store_file_path))
