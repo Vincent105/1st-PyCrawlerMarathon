063a4268693c028d57edfd2f03a179ad2518dba8
vincent105 <vincent1050917@gmail.com>
2020-02-15 19:36:00 +0800

    0215

--- test.py
+++ test.py
@@ -25,27 +25,34 @@ while keepGoing:
     news_block = soup.find_all('div', class_='sc-eEieub sc-iuDHTM ibJqYc')
 
     for news_item in news_block:
-        url = news_item.find('a', class_='sc-RbTVP sc-kEmuub eSJXii').get("href")
+        url = news_item.find(
+            'a', class_='sc-RbTVP sc-kEmuub eSJXii').get("href")
         article_content.setdefault(article_count, {})
 
         if url not in article_url and article_count < target_count:
             article_url.append(url)
             article_content[article_count]['標題'] = news_item.find('h6').string
-            article_content[article_count]['分類'] = news_item.find('div', class_='sc-gacfCG bPSpUf').string
-            article_content[article_count]['摘要'] = news_item.find('p', class_='sc-FQuPU sc-ciodno bvnzOw').string
+            article_content[article_count]['分類'] = news_item.find(
+                'div', class_='sc-gacfCG bPSpUf').string
+            article_content[article_count]['摘要'] = news_item.find(
+                'p', class_='sc-FQuPU sc-ciodno bvnzOw').string
             article_content[article_count]['連結'] = url
             article_count += 1
-        elif url not in article_url and article_count == target_count:    
+        elif url not in article_url and article_count == target_count:
             article_content[article_count]['標題'] = news_item.find('h6').string
-            article_content[article_count]['分類'] = news_item.find('div', class_='sc-gacfCG bPSpUf').string
-            article_content[article_count]['摘要'] = news_item.find('p', class_='sc-FQuPU sc-ciodno bvnzOw').string
-            article_content[article_count]['連結'] = url               
+            article_content[article_count]['分類'] = news_item.find(
+                'div', class_='sc-gacfCG bPSpUf').string
+            article_content[article_count]['摘要'] = news_item.find(
+                'p', class_='sc-FQuPU sc-ciodno bvnzOw').string
+            article_content[article_count]['連結'] = url
             keepGoing = False
 
     time.sleep(2)
     browser.execute_script("window.scrollBy(0,10000)")
 
 # print(article_content)
+pd.set_option('display.max_columns', 5)  # a就是你要设置显示的最大列数参数
+
 
 frame = pd.DataFrame(article_content)
-print(frame.T)
+frame.T
